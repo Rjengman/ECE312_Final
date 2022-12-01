30cf3bed7c70817e28235603d7663f690482effe
Ryan E <rjhengman@outlook.com>
2022-12-01 13:18:44 -0700

    Files filled with skeleton code

---
+++ main.c
@@ -0,0 +1,23 @@
+/* 
+ * File:   Final_main.c
+ * Author: Rjhen
+ *
+ * Created on December 1, 2022, 12:37 PM
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include "defines.h"
+#include "hd44780.h"
+#include "lcd.h"
+
+int main(int argc, char** argv) {
+    
+    
+    
+    return (EXIT_SUCCESS);
+}
